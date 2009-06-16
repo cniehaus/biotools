aeea6aaba71014921b9d3f77ed77a490f35123f1
Carsten Niehaus <cniehaus@kde.org>
2009-06-16 18:23:45 +0200

    Erste Version des Notenprogramms

---
+++ noten/datenklassen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009 Carsten Niehaus. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 2 of the License, or
+# version 3 of the License, or (at your option) any later version. It is
+# provided for educational purposes and is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+
+class Halbjahr(object):
+	def __init__(self):
+		print "Halbjahr"
+
+class Klasse(object):
+	def __init__(self):
+		print "Klasse"
+		
+class Schueler(object):
+	def __init__(self):
+		print "Schüler"
+		
+class Unterrichtsplanung(object):
+	def __init__(self):
+		print "Unterrichtsplanung"
+		
+class Termin(object):
+	def __init__(self):
+		print "Termin"
---
+++ noten/noten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009 Carsten Niehaus. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 2 of the License, or
+# version 3 of the License, or (at your option) any later version. It is
+# provided for educational purposes and is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+
+import csv
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from ui_maindlg import Ui_MainDlg 
+from datenklassen import *
+
+class MainDialog(QDialog, Ui_MainDlg):
+	def __init__(self, parent=None):
+		super(MainDialog, self).__init__(parent)
+		self.setupUi(self)
+		#self.updateUi()
+
+
+
+if __name__ == "__main__":
+	import sys
+	app = QApplication(sys.argv)
+	form = MainDialog()
+	form.show()
+	app.exec_()
+
