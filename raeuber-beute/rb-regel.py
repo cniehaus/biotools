6552712c2e4985f2f7d834dc7d1e21ad012f1682
Carsten Niehaus <cniehaus@kde.org>
2009-07-01 20:41:55 +0200

    Nun ist das eine Klasse --> Damit kann ich den Code gleich im Hauptprogramm nutzen

--- raeuber-beute/rb-regel.py
+++ raeuber-beute/rb-regel.py
@@ -13,47 +13,66 @@ Predator0 = Initial Predator Population;
 dt = Time Step;
 """
 
-a=1.0
-b=0.2
-p=0.04
-c=0.5
-Prey0=5
-Predator0=2
-dt=0.01
-tstart=0.0
-iterations=5000
+class PredatorPreyCalculator(object):
+    def __init__(self):
+        self.a=1.0
+        self.b=0.2
+        self.p=0.04
+        self.c=0.5
+        self.Prey0=5
+        self.Predator0=2
+        self.dt=0.01
+        self.tstart=0.0
+        self.iterations=5000
+    
+    def setA(self, a):
+        self.a = a
+        
+    def setB(self, b):
+        self.b = b
+        
+    def setC(self, c):
+        self.c = c
 
-x = Prey0
-y = Predator0
-t = tstart
+    def setP(self, p):
+        self.p = p
+        
+    def dx(self, x,y):
+        return self.a*x-self.b*x*y
 
-i = 0
+    def dy(self, x,y):
+        return self.p*x*y-self.c*y
+        
+    def calculate(self):
+        r_string = "R, "
+        b_string = "B, "
+        i = 0
+        x = self.Prey0
+        y = self.Predator0
+        t = self.tstart
+    
+        while i < self.iterations:
+            t = i * self.dt
+            xnew = x + self.dx(x,y) * self.dt
+            ynew = y + self.dy(x,y) * self.dt
+            
+            x = xnew
+            y = ynew
+            
+            r_string += str(x)
+            r_string += ", "
+            b_string += str(y)
+            b_string += ", "
+            i += 1
+        
+        print(r_string)
+        print(b_string)
+        
+foo = PredatorPreyCalculator()
+foo.calculate()
 
-r_string = "R, "
-b_string = "B, "
 
-def dx(x,y):
-	return a*x-b*x*y
 
-def dy(x,y):
-	return p*x*y-c*y
 
-while i < iterations:
-	#print "Durchlauf %d" % i
-	t = i * dt
-	xnew = x + dx(x,y) * dt
-	ynew = y + dy(x,y) * dt
 
-	x = xnew
-	y = ynew
 
-	r_string += str(x)
-	r_string += ", "
-	
-	b_string += str(y)
-	b_string += ", "
-	
-	i += 1
-
-print(r_string)
-print(b_string)
